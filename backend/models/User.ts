86d7fbc55538443e2caf69665c17664adf9b7d7c
Christian Gawron <gawron.christian@fh-swf.de>
2021-02-13 17:33:14 +0100

    Fix token management (#7)
    * fix: edit available times
    
    * feat: add profile image
    
    * feat: add profile image
    
    * fix: add prompt: "consent" to force issuing a refresh-token
    fix: display error messages of getAvailableTimes()
    fixes #6

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -2,28 +2,32 @@ import * as mongoose from 'mongoose';
 import { genSalt, hash } from "bcryptjs";
 
 export interface GoogleTokens {
-      access_token: string;
-      refresh_token: string;
-      scope: string;
-      expiry_date: number;
+  access_token?: string;
+  refresh_token?: string;
+  scope?: string;
+  expiry_date?: number;
 }
 
 export interface User extends mongoose.Document {
   email: string;
   name: string;
   password: string;
-  user_url: string; 
-  google_tokens: GoogleTokens; 
+  user_url: string;
+  picture_url: string;
+  google_tokens: GoogleTokens;
 };
 
 // User schema for the Database
 const userSchema = new mongoose.Schema<User>(
   {
+    _id: {
+      type: String,
+    },
     email: {
       type: String,
       trim: true,
       required: true,
-      unique: true,
+      unique: false,
       lowercase: true,
     },
     google_tokens: {
@@ -58,6 +62,11 @@ const userSchema = new mongoose.Schema<User>(
       default: "",
       unique: true,
     },
+    picture_url: {
+      type: String,
+      default: "",
+      unique: true,
+    },
     welcome: {
       type: String,
       default: "Willkommen auf meiner Planungsseite",
@@ -72,15 +81,21 @@ userSchema.pre("save", function (next) {
   let user: User = <User>this;
 
   // only hash the password if it has been modified (or is new)
-  if (!user.isModified("password")) return next();
+  if (!user.isModified("password")) {
+    return next();
+  }
 
   // generate a salt
   genSalt(10, (err, salt) => {
-    if (err) return next(err);
+    if (err) {
+      return next(err);
+    }
 
     // hash the password using our new salt
     hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
+      if (err) {
+        return next(err);
+      }
       // override the cleartext password with the hashed one
       user.password = hash;
       next();
