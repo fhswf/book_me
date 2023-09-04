9e359b187ca2f5496fc1ef384172c49ff562ae3f
Christian Gawron <gawron.christian@fh-swf.de>
2023-09-04 10:10:44 +0000

    fix: security updates

--- release.config.js
+++ release.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-    "extends": "semantic-release-monorepo",
+    //"extends": "semantic-release-monorepo",
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
-        "@semantic-release/npm",
         "@semantic-release/git"
     ],
     "preset": "angular"
