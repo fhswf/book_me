c23617ed03c5994374ecf083941cc7bed49495cf
Christian Gawron <gawron.christian@fh-swf.de>
2024-09-22 23:17:21 +0200

    fix(deployment): client deployment
    - fix utility to patch deployment file

--- utility/patch-k8s.js
+++ utility/patch-k8s.js
@@ -12,7 +12,7 @@ const k8sConfig = yaml.load(fs.readFileSync(args[0], 'utf8'));
 
 console.log('Patching k8s config...');
 
-const image_name = k8sConfig.spec.containers[0].image.split(':')[0];
+const image_name = k8sConfig.spec.template.spec.containers[0].image.split(':')[0];
 
 if (git_sha) {
   k8sConfig.spec.template.spec.containers[0].image = `${image_name}:${git_sha}`;
@@ -20,6 +20,6 @@ if (git_sha) {
 else {
   k8sConfig.spec.template.spec.containers[0].image = `${image_name}:${config.version}`;
 }
-console.log(k8sConfig.spec.containers[0].image)
+console.log(k8sConfig.spec.template.spec.containers[0].image)
 /* write the new k8s config */
 fs.writeFileSync(args[0], yaml.dump(k8sConfig));
