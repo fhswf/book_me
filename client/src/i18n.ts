8f67ab268d20518c4de7ebe4007c3b01723e8a73
Christian Gawron <gawron.christian@fh-swf.de>
2024-09-29 18:58:49 +0200

    fix: ui glitches
    - fix re-render of date picker on month change
    - add translations
    - add missing `<Typography>` tags

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -22,10 +22,7 @@ i18n
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
-        fallbackLng: {
-            'de-DE': ['de'],
-            default: 'en'
-        },
+        fallbackLng: 'en',
         debug: true,
 
         backend: {
