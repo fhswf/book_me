a6d28c6e4e3017c9716f2328e784fadc7c30550c
Christian Gawron <gawron.christian@fh-swf.de>
2024-10-03 13:33:15 +0200

    fix(test): test before sonarqube analysis
    - migrate to CTRF reports

--- common/jest.config.ts
+++ common/jest.config.ts
@@ -2,6 +2,10 @@ import type { Config } from 'jest';
 
 const config: Config = {
   verbose: true,
+  reporters: [
+    'default',
+    ['jest-ctrf-json-reporter', {}],
+  ],
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
