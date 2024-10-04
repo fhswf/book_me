79c4584e57c0f462c82ec3640fbd6b13faaeb311
Christian Gawron <gawron.christian@fh-swf.de>
2024-10-04 15:52:48 +0200

    fix(security): remove password attribute
    - since authentication is handled externally, the `password` attribute is no longer needed

--- backend/src/handlers/validation.ts
+++ backend/src/handlers/validation.ts
@@ -6,17 +6,10 @@ export const validateRegister = [
     .isLength({ min: 4, max: 20 })
     .withMessage("Name must be between 4 and 20 characters!"),
   check("email").isEmail(),
-  check("password", "password is requiered").notEmpty(),
-  check("password")
-    .isLength({ min: 8 })
-    .withMessage("Password must have atleast 8 characters")
-    .matches(/\d/)
-    .withMessage("password must have a number"),
 ];
 
 //Validates the User input before login
 export const validateLogin = [
   check("email").isEmail(),
-  check("password", "password is requiered").notEmpty(),
 ];
 
