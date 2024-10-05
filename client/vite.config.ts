542cf93830992589da7b68ca4d48d8a47f15bf80
Christian Gawron <gawron.christian@fh-swf.de>
2024-10-05 11:22:34 +0200

    fix(test): coverage for cypress tests
    - fix configuration to enable coverage in cypress tests

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import istanbul from 'vite-plugin-istanbul';
+import cypress from 'cypress';
 
 export default defineConfig(() => {
   return {
+    plugins: [react(), istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'test/'],
+      extension: ['.js', '.ts', '.tsx'],
+      requireEnv: true,
+    }),],
     build: {
       outDir: 'build',
       publicDir: 'public',
-      plugins: [react()],
+      sourcemap: true,
       base: '/',
       rollupOptions: {
         onwarn(warning, warn) {
