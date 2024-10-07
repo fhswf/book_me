e318622b468f49b4a54e9b3d29b501b09472b34f
Christian Gawron <gawron.christian@fh-swf.de>
2024-10-07 18:42:41 +0200

    fix: sonarqube issues
    - add cypress test case for calendar integration

--- client/cypress/e2e/app.cy.js
+++ client/cypress/e2e/app.cy.js
@@ -28,7 +28,7 @@ context('Main page', () => {
       cy.intercept('/api/v1/events/getEvents', { fixture: 'events' }).as('getEvents')
     })
 
-    it('Check simple schedule flow', () => {
+    it('Check add event type', () => {
       cy.visit('/app')
       cy.wait(['@getUser'], { timeout: 10000 })
       cy.wait(['@getEvents'], { timeout: 10000 })
@@ -41,13 +41,50 @@ context('Main page', () => {
     before(() => {
       cy.intercept('/api/v1/users/user', { fixture: 'user' }).as('getUser')
       cy.intercept('/api/v1/events/getEvents', { fixture: 'events' }).as('getEvents')
+      cy.intercept('/api/v1/events/getEvent/66e41e641f4f81ece1828ab5', { fixture: 'sprechstunde' }).as('getEvent')
     })
 
-    it('Check simple schedule flow', () => {
+    it('Check edit event type', () => {
       cy.visit('/app')
       cy.wait(['@getUser'], { timeout: 10000 })
       cy.wait(['@getEvents'], { timeout: 10000 })
       cy.get('[data-testid="edit-event-button"]').click()
+      cy.wait(['@getEvent'], { timeout: 10000 })
+      cy.get('[data-testid="event-form-title"] input').should('have.value', 'Sprechstunde')
+    })
+  })
+
+  describe('Visit app main page & log out', () => {
+    before(() => {
+      cy.intercept('/api/v1/users/user', { fixture: 'user' }).as('getUser')
+      cy.intercept('/api/v1/events/getEvents', { fixture: 'events' }).as('getEvents')
+      cy.intercept('/api/v1/events/getEvent/66e41e641f4f81ece1828ab5', { fixture: 'sprechstunde' }).as('getEvent')
+    })
+
+    it('Check log out', () => {
+      cy.visit('/app')
+      cy.wait(['@getUser'], { timeout: 10000 })
+      cy.wait(['@getEvents'], { timeout: 10000 })
+      cy.get('[data-testid="profile-menu"]').click()
+      cy.get('[data-testid="logout-button"]').click()
+      cy.location('pathname').should('eq', '/landing')
+    })
+  })
+
+  describe('Visit app main page & open calendar integration', () => {
+    before(() => {
+      cy.intercept('/api/v1/users/user', { fixture: 'user' }).as('getUser')
+      cy.intercept('/api/v1/events/getEvents', { fixture: 'events' }).as('getEvents')
+      cy.intercept('/api/v1/events/getEvent/66e41e641f4f81ece1828ab5', { fixture: 'sprechstunde' }).as('getEvent')
+    })
+
+    it('Check log out', () => {
+      cy.visit('/app')
+      cy.wait(['@getUser'], { timeout: 10000 })
+      cy.wait(['@getEvents'], { timeout: 10000 })
+      cy.get('[data-testid="profile-menu"]').click()
+      cy.get('[data-testid="calendar-button"]').click()
+      cy.location('pathname').should('eq', '/integration')
     })
   })
 
