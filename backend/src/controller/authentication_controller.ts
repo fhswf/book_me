fbd27066b4e3d016c4053197b551d46555df68c1
Christian Gawron <gawron.christian@fh-swf.de>
2024-10-10 13:35:03 +0200

    feat: store access token in cookie

--- backend/src/controller/authentication_controller.ts
+++ backend/src/controller/authentication_controller.ts
@@ -228,10 +228,14 @@ export const googleLoginController = (req: Request, res: Response): void => {
               }
             );
 
-            res.status(200).json({
-              access_token,
-              user: { _id, email, name, picture_url: picture },
-            });
+            res
+              .cookie('access_token',
+                access_token, { maxAge: 60 * 60 * 24 * 1000, httpOnly: true, secure: true, sameSite: 'strict' })
+              .status(200)
+              .json({
+                access_token,
+                user: { _id, email, name, picture_url: picture },
+              });
           })
           .catch(error => {
             console.error('Error saving user: %o', error);
