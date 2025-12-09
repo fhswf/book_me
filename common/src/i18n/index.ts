ba21d93db3826e3ea2944da115e6ae64ad6abeae
Christian Gawron <gawron.christian@fh-swf.de>
2025-12-09 23:05:03 +0000

    feat: Add French, Italian, Japanese, Korean, Chinese, and Spanish locales and update i18n integration.

--- common/src/i18n/index.ts
+++ common/src/i18n/index.ts
@@ -1,5 +1,11 @@
 import { en } from './locales/en.js';
 import { de } from './locales/de.js';
+import { fr } from './locales/fr.js';
+import { es } from './locales/es.js';
+import { it } from './locales/it.js';
+import { zh } from './locales/zh.js';
+import { ko } from './locales/ko.js';
+import { ja } from './locales/ja.js';
 
 export const resources = {
     en: {
@@ -7,6 +13,24 @@ export const resources = {
     },
     de: {
         translation: de
+    },
+    fr: {
+        translation: fr
+    },
+    es: {
+        translation: es
+    },
+    it: {
+        translation: it
+    },
+    zh: {
+        translation: zh
+    },
+    ko: {
+        translation: ko
+    },
+    ja: {
+        translation: ja
     }
 };
 
